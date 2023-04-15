31524b350a7dd0ccaccf9cbe294b19e7eec72db2
michealhansun <hansun@live.com>
2023-04-15 14:12:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,5 @@
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
+echo 'src-git luci1 https://github.com/openwrt/luci' >>feeds.conf.default
+echo 'src-git packages1 https://github.com/openwrt/packages' >>feeds.conf.default
