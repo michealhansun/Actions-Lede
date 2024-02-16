03ef47a88fb9356d1dc125b74541f230ff845a70
michealhansun <hansun@live.com>
2024-02-16 16:12:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-echo 'src-git luci1 https://github.com/openwrt/luci' >>feeds.conf.default
-echo 'src-git packages1 https://github.com/openwrt/packages' >>feeds.conf.default
+# echo 'src-git luci1 https://github.com/openwrt/luci' >>feeds.conf.default
+# echo 'src-git packages1 https://github.com/openwrt/packages' >>feeds.conf.default
