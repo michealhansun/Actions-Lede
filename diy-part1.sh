1ccb26c86373ed619c54ac4d310cb8f1d9c7332e
michealhansun <hansun@live.com>
2024-02-23 23:04:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,3 +28,4 @@ echo 'src-git autotimeset https://github.com/sirpdboy/luci-app-autotimeset' >>fe
 echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
 echo 'src-git eqosplus https://github.com/sirpdboy/luci-app-eqosplus' >>feeds.conf.default
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
+echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >>feeds.conf.default
